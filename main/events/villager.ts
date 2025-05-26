dbc57b531decd556848cf1dad0caf188b1976c3a
kykyerwgame <160799416+kykyerwgame@users.noreply.github.com>
2025-05-27 06:23:53 +0800

    Update villager.ts

--- main/events/villager.ts
+++ main/events/villager.ts
@@ -1,5 +1,5 @@
 import { RpgEvent, EventData, RpgPlayer } from '@rpgjs/server'
-
+//匯入 RPGJS 的核心
 @EventData({
     name: 'EV-1', 
     hitbox: {
@@ -7,12 +7,18 @@ import { RpgEvent, EventData, RpgPlayer } from '@rpgjs/server'
         height: 16
     }
 })
+//name: EV-1：此事件的 ID ，與地圖上的事件名稱對應。
+//hitbox：碰撞區域大小（站在地圖上，玩家靠近這個 hitbox 才能互動）。
+    
 export default class VillagerEvent extends RpgEvent {
+//建立一個事件類別並預設匯出。這個事件在地圖中代表一位 NPC
     onInit() {
         this.setGraphic('female')
+//設置圖像為femal
     }
     async onAction(player: RpgPlayer) {
-        await player.showText('I give you 10 gold.', {
+//onAction 是玩家與 NPC 互動時觸發的事件
+        await player.showText('我給您10元', {
             talkWith: this
         })
         player.gold += 10
